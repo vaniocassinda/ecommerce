f019afb41f2b33a7235da2c828334af3eb15d046
Vanio Cassinda <vanilsonvanio23@gmail.com>
2020-06-03 20:27:24 +0100

    Meus pedidos e Resumo Carrinho

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Hcode\Model\User;  
+use \Hcode\Model\Cart;  
 
 function formatPrice($vlprice)
 {
@@ -26,5 +27,29 @@ function getUserName()
     return $user->getdesperson();
 
 }
+
+function getCartNrQtd()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+
+}
+
+function getCartVlSubtotal()
+{
+
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
+
+}
+
+
 
  ?>
